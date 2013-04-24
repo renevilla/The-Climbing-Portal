d0710b743d6291875bc7a8dc4aea125d40f44725
Rene Villa <renevilla23@gmail.com>
2013-04-24 16:54:42 -0700

    The Climbing Portal

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TheClimbingPortal::Application.config.secret_token = 'e87bb60ec10a812b6b3a27cac842f4ec0ca292b51b2d42e5694edb28456a49eab5ca14e67169d95917f4c61dc589a4611658b3f687f64001c2c7704e2539f35c'
